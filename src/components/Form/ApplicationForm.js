1e79df2e1a49a4b02995ab3ca0a28495faea3e38
stholmes <stephenshigemi@gmail.com>
2021-11-08 14:03:32 -0800

    Add styles to form component

--- src/components/Form/ApplicationForm.js
+++ src/components/Form/ApplicationForm.js
@@ -10,6 +10,14 @@ const ApplicationForm = () => {
         <div className={classes.new_application}>
             <h2>Add new application</h2>
             <form id="add_application" onSubmit={onSubmitHandler}>
+                <div className={classes.form_control}>
+                    <label for="position" className={classes.form_control__label}>Position</label>
+                    <input type="text" id="position" className={classes.form_control__input} />
+                </div>
+                <div className={classes.form_control}>
+                    <label for="company" className={classes.form_control__label}>Company</label>
+                    <input type="text" id="company" className={classes.form_control__input} />
+                </div>
                 <button type="submit">Add Application</button>
             </form>
         </div>
